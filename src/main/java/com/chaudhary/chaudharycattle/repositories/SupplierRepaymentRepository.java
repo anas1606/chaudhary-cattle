a6976b8569a98f68d3d4c3a75a21abcd4a2e0845
anasloriya <anasloriya.info@gmail.com>
2023-04-17 12:44:55 +0530

    Bug fix

--- src/main/java/com/chaudhary/chaudharycattle/repositories/SupplierRepaymentRepository.java
+++ src/main/java/com/chaudhary/chaudharycattle/repositories/SupplierRepaymentRepository.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public interface SupplierRepaymentRepository extends JpaRepository <SupplierRepayment, Long> {
 
     Page<SupplierRepayment> findAllByCreatedDateBetween (LocalDate startDate, LocalDate endDate, Pageable page);
-    @Query("SELECT sr FROM SupplierRepayment sr WHERE Suppl BETWEEN ?1 AND ?2 AND bId = ?3")
+    @Query("SELECT sr FROM SupplierRepayment sr WHERE createdDate BETWEEN ?1 AND ?2 AND bId = ?3")
     Page<SupplierRepayment> findAllByCreatedDateBetweenAndBId (LocalDate startDate, LocalDate endDate, Supplier supplier, Pageable page);
     @Query(value = "SELECT IFNULL(ROUND(SUM(amount),2),0.0) FROM SupplierRepayment sr WHERE createdDate BETWEEN ?1 AND ?2",nativeQuery = true)
     double sumAmountByCreatedDateBetween (LocalDate startDate, LocalDate endDate);
